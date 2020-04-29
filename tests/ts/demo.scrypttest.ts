311e301cb2d815faa6fb76572c795c4bacc6097d
Xiaohui <whulxh@gmail.com>
2020-04-29 16:53:10 -0700

    support multiple public functions

--- tests/ts/demo.scrypttest.ts
+++ tests/ts/demo.scrypttest.ts
@@ -7,14 +7,16 @@ describe('Test sCrypt contract Demo In Typescript', () => {
 
   before(() => {
     const Demo = buildContractClass(path.join(__dirname, '../../contracts/demo.scrypt'));
-    demo = new Demo(4, 7);
+    demo = new Demo(7, 4);
   });
 
   it('should return true', () => {
-    expect(demo.unlock(4 + 7)).to.equal(true);
+    expect(demo.add(7 + 4)).to.equal(true);
+    expect(demo.sub(7 - 4)).to.equal(true);
   });
 
   it('should return false', () => {
-    expect(demo.unlock(0)).to.equal(false);
+    expect(demo.add(0)).to.equal(false);
+    expect(demo.sub(1)).to.equal(false);
   });
 });
