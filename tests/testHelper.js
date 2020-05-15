9507c892acd04af7a870d5d81ea1f3b91e67caf9
Xiaohui <whulxh@gmail.com>
2020-05-14 22:52:23 -0700

    add advancedTx() example with ANYONECANPAY

--- tests/testHelper.js
+++ tests/testHelper.js
@@ -1,4 +1,6 @@
 const { bsv } = require('scrypttest');
+const Signature = bsv.crypto.Signature
+const Interpreter = bsv.Script.Interpreter
 
 /**
  * a dummy transaction used in script evaluation
@@ -6,8 +8,7 @@ const { bsv } = require('scrypttest');
 
 const inputIndex = 0
 const inputSatoshis = 100000
-const sighashType = bsv.crypto.Signature.SIGHASH_ALL | bsv.crypto.Signature.SIGHASH_FORKID
-const flags = bsv.Script.Interpreter.SCRIPT_VERIFY_MINIMALDATA | bsv.Script.Interpreter.SCRIPT_ENABLE_SIGHASH_FORKID | bsv.Script.Interpreter.SCRIPT_ENABLE_MAGNETIC_OPCODES | bsv.Script.Interpreter.SCRIPT_ENABLE_MONOLITH_OPCODES
+const flags = Interpreter.SCRIPT_VERIFY_MINIMALDATA | Interpreter.SCRIPT_ENABLE_SIGHASH_FORKID | Interpreter.SCRIPT_ENABLE_MAGNETIC_OPCODES | Interpreter.SCRIPT_ENABLE_MONOLITH_OPCODES
 
 const utxo = {
   txId: 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458',
@@ -17,9 +18,9 @@ const utxo = {
 }
 const tx = new bsv.Transaction().from(utxo)
 
-getPreimage = (tx, lockingScript) => bsv.Transaction.sighash.sighashPreimage(tx, sighashType, inputIndex, bsv.Script.fromASM(lockingScript), new bsv.crypto.BN(inputSatoshis), flags)
+getPreimage = (tx, lockingScript, sighashType = Signature.SIGHASH_ALL | Signature.SIGHASH_FORKID) => bsv.Transaction.sighash.sighashPreimage(tx, sighashType, inputIndex, bsv.Script.fromASM(lockingScript), new bsv.crypto.BN(inputSatoshis), flags)
 
-signTx = (tx, privateKey, scriptPubKey) => bsv.Transaction.sighash.sign(tx, privateKey, sighashType, inputIndex, bsv.Script.fromASM(scriptPubKey), new bsv.crypto.BN(inputSatoshis), flags).toTxFormat()
+signTx = (tx, privateKey, scriptPubKey, sighashType = Signature.SIGHASH_ALL | Signature.SIGHASH_FORKID) => bsv.Transaction.sighash.sign(tx, privateKey, sighashType, inputIndex, bsv.Script.fromASM(scriptPubKey), new bsv.crypto.BN(inputSatoshis), flags).toTxFormat()
 
 toHex = x => x.toString('hex')
 
