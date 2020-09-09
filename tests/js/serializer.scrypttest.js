e2f4236275bdf6696c3ee4c18498b49461754fe9
Xiaohui <whulxh@gmail.com>
2020-09-09 16:22:27 -0700

    randomize inputs

--- tests/js/serializer.scrypttest.js
+++ tests/js/serializer.scrypttest.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const { buildContractClass, Bytes } = require('scryptlib');
 const { compileContract } = require('../../helper');
 
-describe('Test sCrypt contract Demo In Javascript', () => {
+describe('Test sCrypt contract Serializer In Javascript', () => {
   let demo, result
 
   before(() => {
@@ -11,7 +11,17 @@ describe('Test sCrypt contract Demo In Javascript', () => {
   });
 
   it('should return true', () => {
-    result = demo.main(false, new Bytes('234f'), 3412435).verify()
-    expect(result.success, result.error).to.be.true
+    const getRandomInt = digits => Math.floor(Math.random() * 2 ** digits)
+    const getRandomHex = digits => [...Array(digits)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+        
+    for (i = 0; i < 100; i++) {
+        const digits = getRandomInt(6) + 1
+        const n = getRandomInt(digits)
+        // bytes must have even hex digits
+        const h = getRandomHex(digits * 2)
+        
+        result = demo.main(n % 2 === 0, new Bytes(h), n).verify()
+        expect(result.success, result.error).to.be.true
+    }
   });
 });
