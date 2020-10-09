e3ddf0c4b43c0d4be6748bedc4d961b7969f3ed4
Merlin Buczek <merlin.buczek@pm.me>
2020-10-09 21:35:56 +0200

    Add merkleToken buy tests

--- tests/js/merkleToken.scrypttest.js
+++ tests/js/merkleToken.scrypttest.js
@@ -4,82 +4,128 @@ const { inputIndex, inputSatoshis, tx, compileContract, dummyTxId } = require(".
 const crypto = require("crypto")
 
 function sha256(x) {
-  return crypto.createHash("sha256").update(x).digest("hex")
+  return crypto.createHash("sha256").update(Buffer.from(x, "hex")).digest("hex")
 }
 
 describe("Test sCrypt contract merkleToken In Javascript", () => {
-  let token, lockingScriptCodePart
-
-  const tx_ = new bsv.Transaction()
   const Signature = bsv.crypto.Signature
   const privateKey = new bsv.PrivateKey.fromRandom("testnet")
+  // const privateKey = new bsv.PrivateKey.fromString("Kyv5ikUuSt1jDjvcoFCPR9NzFif8N3vNtAFcwZXw7WpAwaK9SoC2")
   const publicKey = bsv.PublicKey.fromPrivateKey(privateKey)
   const satPrice = 100
-  const oldEntry = toHex("01" + publicKey.toAddress().toHex())
   const sighashType = Signature.SIGHASH_ANYONECANPAY | Signature.SIGHASH_ALL | Signature.SIGHASH_FORKID
 
-  before(() => {
-    const Token = buildContractClass(compileContract("merkleToken.scrypt"))
+  const pkh = bsv.crypto.Hash.sha256ripemd160(publicKey.toBuffer())
+  const changeAddress = toHex(pkh) // Needs to be unprefixed address
+  const payoutAddress = changeAddress
+  const changeSats = 100
+
+  const Token = buildContractClass(compileContract("merkleToken.scrypt"))
+
+  let token, lockingScriptCodePart, tx_
+
+  beforeEach(() => {
+    tx_ = new bsv.Transaction()
     token = new Token(satPrice)
 
     lockingScriptCodePart = token.codePart.toASM()
   })
 
   it("should buy token", () => {
-    function testBuyMore(amount, changeAddress, payoutAddress, changeSats, prevBalance, merklePath) {
-      token.dataLoad = toHex(sha256(sha256(oldEntry).repeat(2)))
+    const amount = 1
+    const newEntry = toHex(payoutAddress + num2bin(amount, 1))
+    const lastEntry = toHex("00".repeat(20) + "01")
+    const newLockingScript = lockingScriptCodePart + " OP_RETURN " + sha256(sha256(lastEntry) + sha256(newEntry))
+    const lastMerklePath = new Bytes(sha256(lastEntry) + "01")
 
-      tx_.addInput(
-        new bsv.Transaction.Input({
-          prevTxId: dummyTxId,
-          outputIndex: 0,
-          script: ""
-        }),
-        bsv.Script.fromASM(token.lockingScript.toASM()),
-        inputSatoshis
+    token.dataLoad = sha256(sha256(lastEntry).repeat(2))
+
+    tx_.addInput(
+      new bsv.Transaction.Input({
+        prevTxId: dummyTxId,
+        outputIndex: 0,
+        script: ""
+      }),
+      bsv.Script.fromASM(token.lockingScript.toASM()),
+      inputSatoshis
+    )
+
+    // token output
+    tx_.addOutput(
+      new bsv.Transaction.Output({
+        script: bsv.Script.fromASM(newLockingScript),
+        satoshis: inputSatoshis + satPrice * amount
+      })
+    )
+
+    // change output
+    tx_.addOutput(
+      new bsv.Transaction.Output({
+        script: bsv.Script.buildPublicKeyHashOut(publicKey.toAddress()),
+        satoshis: changeSats
+      })
+    )
+
+    const preimage = getPreimage(tx_, token.lockingScript.toASM(), inputSatoshis, inputIndex, sighashType)
+
+    token.txContext = { tx: tx_, inputIndex, inputSatoshis }
+    const result = token
+      .buy(
+        new SigHashPreimage(toHex(preimage)),
+        amount,
+        new Ripemd160(changeAddress),
+        new Ripemd160(payoutAddress),
+        changeSats,
+        new Bytes(lastEntry),
+        lastMerklePath
       )
+      .verify()
 
-      // tx_.addInput(
-      //   new bsv.Transaction.Input({
-      //     prevTxId: dummyTxId,
-      //     outputIndex: 1,
-      //     script: ""
-      //   }),
-      //   bsv.Script.buildPublicKeyHashOut(privateKey.toAddress()),
-      //   // bsv.Script.fromASM(token.lockingScript.toASM()),
-      //   200
-      // )
+    expect(result.success, result.error).to.be.true
+  })
 
-      tokenBalance = num2bin(amount, 1)
+  it("should buy more tokens", () => {
+    const amount = 1
+    const prevBalance = 1
+    const oldEntry = toHex(payoutAddress + "01")
+    const tokenBalance = num2bin(amount + 1, 1)
+    const newEntry = toHex(payoutAddress + tokenBalance)
+    const newLockingScript = lockingScriptCodePart + " OP_RETURN " + sha256(sha256(newEntry).repeat(2))
+    const merklePath = new Bytes(sha256(oldEntry) + "01")
 
-      const newEntry = toHex(tokenBalance + publicKey.toAddress().toHex())
+    token.dataLoad = sha256(sha256(oldEntry).repeat(2))
 
-      const newLockingScript = lockingScriptCodePart + " OP_RETURN " + sha256(sha256(newEntry).repeat(2))
+    tx_.addInput(
+      new bsv.Transaction.Input({
+        prevTxId: dummyTxId,
+        outputIndex: 0,
+        script: ""
+      }),
+      bsv.Script.fromASM(token.lockingScript.toASM()),
+      inputSatoshis
+    )
 
-      // token output
-      tx_.addOutput(
-        new bsv.Transaction.Output({
-          script: bsv.Script.fromASM(newLockingScript),
-          satoshis: inputSatoshis + satPrice * amount
-        })
-      )
+    // token output
+    tx_.addOutput(
+      new bsv.Transaction.Output({
+        script: bsv.Script.fromASM(newLockingScript),
+        satoshis: inputSatoshis + satPrice * amount
+      })
+    )
 
-      // change output
-      tx_.addOutput(
-        new bsv.Transaction.Output({
-          script: bsv.Script.buildPublicKeyHashOut(privateKey.toAddress()),
-          satoshis: changeSats
-        })
-      )
+    // change output
+    tx_.addOutput(
+      new bsv.Transaction.Output({
+        script: bsv.Script.buildPublicKeyHashOut(publicKey.toAddress()),
+        satoshis: changeSats
+      })
+    )
 
-      const preimage = getPreimage(tx_, token.lockingScript.toASM(), inputSatoshis, inputIndex, sighashType)
+    const preimage = getPreimage(tx_, token.lockingScript.toASM(), inputSatoshis, inputIndex, sighashType)
 
-      // console.log(toHex(sha256(sha256(oldEntry).repeat(2)))) // OP_RETURN attachment
-      // console.log(toHex(preimage)) // preimage
-      // console.log(tx_.toString()) // transaction hex
-
-      token.txContext = { tx: tx_, inputIndex, inputSatoshis }
-      return token.buyMore(
+    token.txContext = { tx: tx_, inputIndex, inputSatoshis }
+    const result = token
+      .buyMore(
         new SigHashPreimage(toHex(preimage)),
         amount,
         new Ripemd160(changeAddress),
@@ -88,18 +134,8 @@ describe("Test sCrypt contract merkleToken In Javascript", () => {
         prevBalance,
         merklePath
       )
-    }
+      .verify()
 
-    let amount, changeAddress, payoutAddress, changeSats, prevBalance, merklePath
-
-    amount = 1
-    changeAddress = publicKey.toAddress().toHex()
-    payoutAddress = changeAddress
-    changeSats = 100
-    prevBalance = 1
-    merklePath = new Bytes([sha256(sha256(oldEntry) + sha256(oldEntry)), "01"].join(""))
-
-    result = testBuyMore(amount, changeAddress, payoutAddress, changeSats, prevBalance, merklePath).verify()
     expect(result.success, result.error).to.be.true
   })
 })
