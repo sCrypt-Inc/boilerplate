90edf7505c85e669e06aee5dbe14244eb9e2f3fa
ccoin <ccoincash@gmail.com>
2020-11-09 11:14:21 +0800

    remove useless log

--- deployments/binaryoption.js
+++ deployments/binaryoption.js
@@ -11,7 +11,6 @@ const {
     try {
         
         let betPrice = 100
-        //let rabinPubKey = 0x15525796ddab817a3c54c4bea4ef564f090c5909b36818c1c13b9e674cf524aa3387a408f9b63c0d88d11a76471f9f2c3f29c47a637aa60bf5e120d1f5a65221n
         let rabinPubKey = 0x1541942cc552a95c4832350ce99c2970f5b3ce9237a09c70c0e867d28039c05209b601105d3b3634cdaee4931809bc0c41d6165a0df16829a3a31202f56003239dd2c6e12297e94ef03e6aa61a147ea2b51c476dc45f5a2406b66d1ece2755c1f3d4144c0a42acc99b599d0643654a4cac392efbcf3db84d4233834afd1n
         // 2021.1.1 00:00:00
         let timestamp = 1609430400 
@@ -20,7 +19,6 @@ const {
 
         let pubKeyHashA = new Ripemd160(toHex(bsv.crypto.Hash.sha256ripemd160(publicKeyA.toBuffer())))
         let pubKeyHashB = new Ripemd160(toHex(bsv.crypto.Hash.sha256ripemd160(publicKeyB.toBuffer())))
-        console.log("pubkeyhash: ", toHex(bsv.crypto.Hash.sha256ripemd160(publicKeyA.toBuffer())), toHex(bsv.crypto.Hash.sha256ripemd160(publicKeyB.toBuffer())))
 
         const BinaryOption = buildContractClass(loadDesc('binaryOption_desc.json'))
         const binaryOption = new BinaryOption(betPrice, rabinPubKey, timestamp, pubKeyHashA, pubKeyHashB)
@@ -31,7 +29,6 @@ const {
         lockingTx.outputs[0].setScript(binaryOption.lockingScript)
         lockingTx.sign(privateKey)
         let lockingTxid = await sendTx(lockingTx)
-        //let lockingTxid = '82d1f53d80c05bdc4617c8d5e857a12807d61551d863eae20172beb55f034fae'
         console.log('binaryoption txid: ', lockingTxid)
 
         let prevLockingScript = binaryOption.lockingScript.toASM();
@@ -42,19 +39,14 @@ const {
 
         const preimage = getPreimage(unlockingTx, prevLockingScript, amount)
         let sig = 0x64b087945b9b49fb1dce3994bb9da714401edd75d6887c4bbf2864cd052e296755580e91ee539f5c24bb32b8fb7266e0effd0c5194b3c4af967b4341784bc3ecf377d46c56f433bd802d73739fe18f11322ee8f84c17bee4eb6d65514e30bbebdc19f428605bcae93f0558995aa3ac5dd09363a7dbaeecd0d3a052feb8n 
-        console.log('rabind:', rabinPubKey, sig)
+        //console.log('rabind:', rabinPubKey, sig)
         let msg = new Bytes('500000000000000080f5ed5f00000000')
         let padding = new Bytes('00')
         const unlockingScript = binaryOption.unlock(new SigHashPreimage(toHex(preimage)), sig, msg, padding, newAmount).toScript()
-        console.log('preimage %s, json: %s, sig %s', toHex(preimage), preimage.toJSON(), sig)
+        //console.log('preimage %s, json: %s, sig %s', toHex(preimage), preimage.toJSON(), sig)
         unlockingTx.inputs[0].setScript(unlockingScript)
-        console.log('unlocking tx:', toHex(unlockingTx.serialize()))
+        //console.log('unlocking tx:', toHex(unlockingTx.serialize()))
         let input = unlockingTx.inputs[0]
-        console.log('scriptCode:', input.script.toHex())
-        console.log('prevLockingScript:', binaryOption.lockingScript.toHex())
-        console.log('unlockingScript:', unlockingScript.toHex())
-        console.log('unlockingScript ASM:', unlockingScript.toASM())
-
 
         unlockingTxid = await sendTx(unlockingTx)
         console.log('unlocking tx:', unlockingTxid)
