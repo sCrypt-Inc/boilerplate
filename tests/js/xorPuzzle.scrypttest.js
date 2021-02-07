592a8c3254422c5eafdb1e6910cfa3e9560653fd
Dirk Brockhausen <bro@brocon.com>
2021-02-07 18:46:41 +0100

    Security enhancements.  Using: xorResult = data ^ sha256(pubKey + data) instead of: xorResult = data ^ pubKey

--- tests/js/xorPuzzle.scrypttest.js
+++ tests/js/xorPuzzle.scrypttest.js
@@ -23,13 +23,13 @@ const {
 const data = '9999';
 const dataBuf = Buffer.from(data);
 const dataBufHash = bsv.crypto.Hash.sha256(dataBuf);
-const dataBufHashHex = toHex(dataBufHash).padStart(66, '0');
+const dataBufHashHex = toHex(dataBufHash);
 const dataBufHashBI = BigInt('0x' + dataBufHashHex);
 
 const data_false = '9998';
 const dataBuf_false = Buffer.from(data_false);
 const dataBufHash_false = bsv.crypto.Hash.sha256(dataBuf_false);
-const dataBufHashHex_false = toHex(dataBufHash_false).padStart(66, '0');
+const dataBufHashHex_false = toHex(dataBufHash_false);
 
 // for output of locking transaction
 const privateKeyA = new bsv.PrivateKey.fromRandom('testnet');
@@ -38,12 +38,16 @@ const publicKeyA = privateKeyA.publicKey;
 const publicKeyAHex = toHex(publicKeyA);
 const publicKeyABI = BigInt('0x' + publicKeyAHex);
 
-const xorResult = dataBufHashBI ^ publicKeyABI;
-let xorResultHex = xorResult.toString(16).padStart(66, '0');
+const publicKeyData = publicKeyAHex + dataBufHashHex;
 
-const privateKeyB = new bsv.PrivateKey.fromRandom('testnet');
-console.log(`Private key generated: '${privateKeyB.toWIF()}'`);
-const addressB = privateKeyB.toAddress();
+const dataBuffer = Buffer.from(publicKeyData, 'hex');
+const publicKeyDataHash = bsv.crypto.Hash.sha256(dataBuffer);
+const publicKeyDataHashHex = toHex(publicKeyDataHash);
+
+const publicKeyDataHashBI = BigInt('0x' + publicKeyDataHashHex);
+
+const xorResult = dataBufHashBI ^ publicKeyDataHashBI;
+let xorResultHex = xorResult.toString(16);
 
 const tx = newTx();
 
@@ -66,16 +70,19 @@ describe('Test sCrypt contract HashPuzzle In Javascript', () => {
     result = xorPuzzle
       .unlock(
         new Sig(toHex(sig)),
+        new PubKey(toHex(publicKeyA)),
         new Bytes(dataBufHashHex)
       )
       .verify();
     expect(result.success, result.error).to.be.true;
   });
-
+  
+  
   it('check should fail when wrong data provided', () => {
     result = xorPuzzle
       .unlock(
         new Sig(toHex(sig)),
+        new PubKey(toHex(publicKeyA)),
         new Bytes(dataBufHashHex_false)
       )
       .verify();
