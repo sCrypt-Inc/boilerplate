5b282e98edab569328ed743af05ee5d19ca18f40
hh <hf.zheng@scrypt.io>
2021-10-23 11:54:06 +0800

    don't test scryptlib in boilerplate

--- tests/js/structdemo.scrypttest.js
+++ tests/js/structdemo.scrypttest.js
@@ -33,71 +33,40 @@ describe('Test sCrypt contract StructDemo In Javascript', () => {
   });
 
 
-  it('should throw', () => {
+  it('should verify failed', () => {
 
-    expect(() => {
-      structDemo = new StructDemo(new Person({
-        name:  1,
-        leftHanded: false,
-        age: 33,
-        addr: new Bytes("68656c6c6f20776f726c6421")
-      }));
-    }).to.throws(/wrong argument type, expected bytes but got int/);
-
-
-    expect(() => {
-      structDemo.main(new Person({
-        leftHanded: false,
-        age: 33,
-        addr: new Bytes("68656c6c6f20776f726c6421")
-      })).verify()
-    }).to.throws(/argument of type struct Person missing member name/);
-
-    expect(() => {
-      structDemo.main(new Person({
-        id: 01,
-        name: new Bytes("7361746f736869206e616b616d6f746f"),
-        leftHanded: false,
-        age: 33,
-        addr: new Bytes("68656c6c6f20776f726c6421")
-      })).verify()
-    }).to.throws(/id is not a member of struct Person/);
-
-
-    it('should verify failed', () => {
-
-      result = structDemo.main(new Person({
-        name: new Bytes("7361746f736869206e616b616d6f746f"),
-        leftHanded: false,
-        age: 32,
-        addr: new Bytes("68656c6c6f20776f726c6421")
-      })).verify()
-      expect(result.success, result.error).to.be.false
-
-    });
-
-    it('should verify failed', () => {
-
-      result = structDemo.main(new Person({
-        name: new Bytes("7361746f736869206e616b616d6f746e"),
-        leftHanded: false,
-        age: 33,
-        addr: new Bytes("68656c6c6f20776f726c6421")
-      })).verify()
-      expect(result.success, result.error).to.be.false
-
-    });
-
-    it('should verify failed', () => {
-
-      result = structDemo.main(new Person({
-        name: new Bytes("7361746f736869206e616b616d6f746f"),
-        leftHande1d: false,
-        age: 33,
-        addr: new Bytes("68656c6c6f20776f726c6420")
-      })).verify()
-      expect(result.success, result.error).to.be.false
-    });
+    result = structDemo.main(new Person({
+      name: new Bytes("7361746f736869206e616b616d6f746f"),
+      leftHanded: false,
+      age: 32,
+      addr: new Bytes("68656c6c6f20776f726c6421")
+    })).verify()
+    expect(result.success, result.error).to.be.false
 
   });
+
+  it('should verify failed', () => {
+
+    result = structDemo.main(new Person({
+      name: new Bytes("7361746f736869206e616b616d6f746e"),
+      leftHanded: false,
+      age: 33,
+      addr: new Bytes("68656c6c6f20776f726c6421")
+    })).verify()
+    expect(result.success, result.error).to.be.false
+
+  });
+
+  it('should verify failed', () => {
+
+    result = structDemo.main(new Person({
+      name: new Bytes("7361746f736869206e616b616d6f746f"),
+      leftHanded: false,
+      age: 33,
+      addr: new Bytes("68656c6c6f20776f726c6420")
+    })).verify()
+    expect(result.success, result.error).to.be.false
+  });
+
+
 });
