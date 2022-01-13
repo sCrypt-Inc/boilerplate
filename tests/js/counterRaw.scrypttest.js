43513a95a21c01df90f0075e7e37f7bf7ff324da
hh <hf.zheng@scrypt.io>
2022-01-13 10:42:14 +0800

    rename state Counter

---
+++ tests/js/counterRaw.scrypttest.js
@@ -0,0 +1,56 @@
+const { expect } = require('chai');
+const { bsv, buildContractClass, getPreimage, toHex, num2bin, SigHashPreimage } = require('scryptlib');
+
+const {
+  inputIndex,
+  inputSatoshis,
+  newTx,
+  DataLen,
+  compileContract
+} = require('../../helper');
+
+const tx = newTx();
+const outputAmount = 222222
+
+describe('Test sCrypt contract Counter In Javascript', () => {
+  let counter, preimage, result
+
+  before(() => {
+    const Counter = buildContractClass(compileContract('counterRaw.scrypt'))
+    counter = new Counter()
+
+    // set initial counter value
+    counter.setDataPart(num2bin(0, DataLen))
+
+    const newLockingScript = [counter.codePart.toASM(), num2bin(1, DataLen)].join(' ')
+
+    tx.addOutput(new bsv.Transaction.Output({
+      script: bsv.Script.fromASM(newLockingScript),
+      satoshis: outputAmount
+    }))
+
+    preimage = getPreimage(tx, counter.lockingScript, inputSatoshis)
+
+    // set txContext for verification
+    counter.txContext = {
+      tx,
+      inputIndex,
+      inputSatoshis
+    }
+  });
+
+  it('should succeed when pushing right preimage & amount', () => {
+    result = counter.increment(new SigHashPreimage(toHex(preimage)), outputAmount).verify()
+    expect(result.success, result.error).to.be.true
+  });
+
+  it('should fail when pushing wrong preimage', () => {
+    result = counter.increment(new SigHashPreimage(toHex(preimage) + '01'), outputAmount).verify()
+    expect(result.success, result.error).to.be.false
+  });
+
+  it('should fail when pushing wrong amount', () => {
+    result = counter.increment(new SigHashPreimage(toHex(preimage)), outputAmount - 1).verify()
+    expect(result.success, result.error).to.be.false
+  });
+});
