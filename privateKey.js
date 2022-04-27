445afd90e29a60e4c72bc2e9dfdffc6861a2a34d
hh <hh@scrypt.io>
2022-04-27 09:27:14 +0800

    update error message

--- privateKey.js
+++ privateKey.js
@@ -14,9 +14,8 @@ function genPrivKey() {
   const newPrivKey = new bsv.PrivateKey.fromRandom('testnet')
   console.log(`Missing private key, generating a new one ...
 Private key generated: '${newPrivKey.toWIF()}'
-You can fund its address '${newPrivKey.toAddress()}' from some faucet and use it to complete the test
-Example faucets are https://faucet.bitcoincloud.net and https://testnet.satoshisvision.network`)
-  exit(0)
+You can fund its address '${newPrivKey.toAddress()}' from sCrypt faucet https://scrypt.io/#faucet`)
+  exit(-1)
 }
 
 const privateKey = new bsv.PrivateKey.fromWIF(key)
