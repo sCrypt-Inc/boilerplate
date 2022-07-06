59d0c268555af4ec24f47e753b13e33be7b36ad1
hh <hh@scrypt.io>
2022-07-06 08:19:56 +0800

    rename to privKey

--- privateKey.js
+++ privateKey.js
@@ -2,9 +2,9 @@ const { exit } = require('process')
 const { bsv } = require('scryptlib');
 
 // fill in private key on testnet in WIF here
-const key = ''
-const key2 = ''
-const key3 = ''
+const privKey = ''
+const privKey2 = ''
+const privKey3 = ''
 
 if (!key) {
   genPrivKey()
@@ -18,11 +18,11 @@ You can fund its address '${newPrivKey.toAddress()}' from sCrypt faucet https://
   exit(-1)
 }
 
-const privateKey = new bsv.PrivateKey.fromWIF(key)
+const privateKey = new bsv.PrivateKey.fromWIF(privKey)
 
-const privateKey2 = key2 ? new bsv.PrivateKey.fromWIF(key2) : privateKey
+const privateKey2 = privKey2 ? new bsv.PrivateKey.fromWIF(privKey2) : privateKey
 
-const privateKey3 = key3 ? new bsv.PrivateKey.fromWIF(key3) : privateKey
+const privateKey3 = privKey3 ? new bsv.PrivateKey.fromWIF(privKey3) : privateKey
 
 //console.log('' + privateKey.toAddress())
 
