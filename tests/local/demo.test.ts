e8632d1b139bfa833608e34dd0aa06554811a47d
hh <hh@scrypt.io>
2022-12-09 23:18:48 +0800

    Fix sCrypt-Inc/scrypt-ts#60

--- tests/local/demo.test.ts
+++ tests/local/demo.test.ts
@@ -18,11 +18,34 @@ describe('Test SmartContract `Demo`', () => {
 
   })
 
-  it('should throw', async () => {
+  it('should pass with negative', async () => {
+    let demo = new Demo(-1n, -2n);
+
+    let result = demo.verify(() => demo.add(-3n));
+    expect(result.success, result.error).to.eq(true);
+
+    result = demo.verify(() => demo.sub(1n));
+    expect(result.success, result.error).to.eq(true);
+
+  })
+
+  it('should pass the non-public method unit test', () => {
     let demo = new Demo(1n, 2n);
+    expect(demo.sum(3n, 4n)).to.be.eq(7n);
+  })
+
+
+  it('should throw error', () => {
 
     expect(() => {
-      demo.add(4n);
+      let demo = new Demo(1n, 2n);
+      demo.add(4n)
     }).to.throw(/Execution failed/)
-  })
+
+    expect(() => {
+      let demo = new Demo(-1n, -2n);
+      demo.add(-4n)
+    }).to.throw(/Execution failed/)
+  });
+
 })
