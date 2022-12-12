1005c4f68d5f4671ed9b6c5b56d85bed2c05dfb5
hh <hh@scrypt.io>
2022-12-12 10:36:34 +0800

    update travis

--- ci-setup.sh
+++ ci-setup.sh
@@ -2,10 +2,12 @@
 set -e
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    echo "osx"
     sed -i '' 's/process.env.PRIVATE_KEY/scrypt_ts_1.bsv.PrivateKey.fromRandom("testnet").toWIF()/' dist/tests/privateKey.js
 fi
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    echo "linux"
     sed -i 's/process.env.PRIVATE_KEY/scrypt_ts_1.bsv.PrivateKey.fromRandom("testnet").toWIF()/' dist/tests/privateKey.js
 fi
 
