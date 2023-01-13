9d43142d2f55548edf44e578478c53198f4aa6cc
Mihael Šinkec <sinkec.mihael@gmail.com>
2023-01-13 12:43:50 +0100

    Added CLTV example.

---
+++ tests/local/cltv.test.ts
@@ -0,0 +1,44 @@
+import { expect } from 'chai'
+import { bsv } from 'scrypt-ts'
+import { CheckLockTimeVerify } from '../../src/contracts/cltv'
+import { dummyUTXO, newTx } from './util/txHelper'
+
+describe('Test SmartContract `CheckLockTimeVerify`', () => {
+    before(async () => {
+        await CheckLockTimeVerify.compile()
+    })
+
+    it('should pass the public method unit test successfully.', async () => {
+        const inputSatoshis = 1000
+        const inputIndex = 0
+        const lockTimeMin = 1673510000n
+        const timeNow = 1673523720
+
+        const cltv = new CheckLockTimeVerify(lockTimeMin)
+
+        const deployTx = cltv.getDeployTx([dummyUTXO], inputSatoshis)
+
+        const callTx = cltv.getCallTxForUnlock(timeNow, deployTx)
+        callTx.seal()
+
+        const result = callTx.verifyInputScript(inputIndex)
+        expect(result.success, result.error).to.eq(true)
+    })
+
+    it('should fail when nLocktime is too low.', async () => {
+        const inputSatoshis = 1000
+        const inputIndex = 0
+        const lockTimeMin = 1673510000n
+        const timeNow = 1673500100
+
+        const cltv = new CheckLockTimeVerify(lockTimeMin)
+
+        const deployTx = cltv.getDeployTx([dummyUTXO], inputSatoshis)
+
+        expect(() => {
+            const callTx = cltv.getCallTxForUnlock(timeNow, deployTx)
+            callTx.seal()
+            callTx.verifyInputScript(inputIndex)
+        }).to.throw(/Execution failed/)
+    })
+})
