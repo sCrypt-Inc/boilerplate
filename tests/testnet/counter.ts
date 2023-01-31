f519b1b87e6ee3cc8392bb6a89d83f43f575f0a8
aaron67 <aaron67@aaron67.cc>
2023-01-31 17:23:57 +0800

    Rebase on master

--- tests/testnet/counter.ts
+++ tests/testnet/counter.ts
@@ -71,8 +71,6 @@ async function main() {
             newCounter.count
         )
 
-        utxoMgr.collectUtxoFrom(callTx)
-
         // prepare for the next iteration
         prevTx = callTx
         prevInstance = newCounter
