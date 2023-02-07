8d8de7e0b244e2009c9cffefa43699c788622791
hh <hh@scrypt.io>
2023-02-08 00:41:29 +0800

    add hashedmap example

---
+++ src/contracts/hashedMapState.ts
@@ -0,0 +1,83 @@
+import {
+    method,
+    prop,
+    SmartContract,
+    assert,
+    hash256,
+    HashedMap,
+    ByteString,
+    SortedItem,
+    SigHash,
+} from 'scrypt-ts'
+
+export class HashedMapState extends SmartContract {
+    @prop(true)
+    hashedmap: HashedMap<bigint, ByteString>
+
+    constructor(hashedmap: HashedMap<bigint, ByteString>) {
+        super(hashedmap)
+        this.hashedmap = hashedmap
+    }
+
+    @method(SigHash.SINGLE)
+    public insert(key: SortedItem<bigint>, val: ByteString) {
+        assert(
+            this.hashedmap.set(key, val),
+            'set key-value pair into hashedMap failed'
+        )
+        assert(
+            this.ctx.hashOutputs ==
+                hash256(this.buildStateOutput(this.ctx.utxo.value)),
+            'hashOutputs check failed'
+        )
+    }
+
+    @method(SigHash.SINGLE)
+    public canGet(key: SortedItem<bigint>, val: ByteString) {
+        assert(this.hashedmap.has(key), `hashedMap does not have key: ${key}`)
+        assert(
+            this.hashedmap.canGet(key, val),
+            `can not get key-value pair: ${key}-${val}`
+        )
+        assert(
+            this.ctx.hashOutputs ==
+                hash256(this.buildStateOutput(this.ctx.utxo.value)),
+            'hashOutputs check failed'
+        )
+    }
+
+    @method(SigHash.SINGLE)
+    public notExist(key: SortedItem<bigint>) {
+        assert(
+            !this.hashedmap.has(key),
+            `key: ${key} should not exist in hashedmap`
+        )
+        assert(
+            this.ctx.hashOutputs ==
+                hash256(this.buildStateOutput(this.ctx.utxo.value)),
+            'hashOutputs check failed'
+        )
+    }
+
+    @method(SigHash.SINGLE)
+    public update(key: SortedItem<bigint>, val: ByteString) {
+        assert(
+            this.hashedmap.set(key, val),
+            'set key-value pair into hashedMap failed'
+        )
+        assert(
+            this.ctx.hashOutputs ==
+                hash256(this.buildStateOutput(this.ctx.utxo.value)),
+            'hashOutputs check failed'
+        )
+    }
+
+    @method(SigHash.SINGLE)
+    public delete(key: SortedItem<bigint>) {
+        assert(this.hashedmap.delete(key), 'delete key from hashedMap failed')
+        assert(
+            this.ctx.hashOutputs ==
+                hash256(this.buildStateOutput(this.ctx.utxo.value))
+        )
+    }
+}
---
+++ tests/testnet/hashedMapState.ts
@@ -0,0 +1,170 @@
+import {
+    ByteString,
+    HashedMap,
+    toByteString,
+    getSortedItem,
+    MethodCallOptions,
+} from 'scrypt-ts'
+import { HashedMapState } from '../../src/contracts/hashedMapState'
+import { getTestnetSigner } from './util/txHelper'
+
+const initBalance = 1
+
+function insert(
+    map: Map<bigint, ByteString>,
+    instance: HashedMapState,
+    key: bigint,
+    val: ByteString
+) {
+    const newInstance = instance.next()
+
+    map.set(key, val)
+    newInstance.hashedmap.attach(map)
+
+    return instance.methods().insert(getSortedItem(map, key), val, {
+        next: {
+            instance: newInstance,
+            balance: initBalance,
+        },
+    } as MethodCallOptions<HashedMapState>)
+}
+
+function update(
+    map: Map<bigint, ByteString>,
+    instance: HashedMapState,
+    key: bigint,
+    val: ByteString
+) {
+    const newInstance = instance.next()
+
+    map.set(key, val)
+    newInstance.hashedmap.attach(map)
+
+    return instance.methods().update(getSortedItem(map, key), val, {
+        next: {
+            instance: newInstance,
+            balance: initBalance,
+        },
+    } as MethodCallOptions<HashedMapState>)
+}
+
+function canGet(
+    map: Map<bigint, ByteString>,
+    instance: HashedMapState,
+    key: bigint,
+    val: ByteString
+) {
+    const newInstance = instance.next()
+
+    return instance.methods().canGet(getSortedItem(map, key), val, {
+        next: {
+            instance: newInstance,
+            balance: initBalance,
+        },
+    } as MethodCallOptions<HashedMapState>)
+}
+
+function deleteKey(
+    map: Map<bigint, ByteString>,
+    instance: HashedMapState,
+    key: bigint,
+    val: ByteString
+) {
+    const newInstance = instance.next()
+    map.delete(key)
+    newInstance.hashedmap.attach(map)
+    return instance.methods().delete(getSortedItem(map, key), {
+        next: {
+            instance: newInstance,
+            balance: initBalance,
+        },
+    } as MethodCallOptions<HashedMapState>)
+}
+
+function notExist(
+    map: Map<bigint, ByteString>,
+    instance: HashedMapState,
+    key: bigint,
+    val: ByteString
+) {
+    const newInstance = instance.next()
+    map.delete(key)
+    newInstance.hashedmap.attach(map)
+    return instance.methods().notExist(getSortedItem(map, key), {
+        next: {
+            instance: newInstance,
+            balance: initBalance,
+        },
+    } as MethodCallOptions<HashedMapState>)
+}
+
+async function main() {
+    await HashedMapState.compile()
+    const signer = await getTestnetSigner()
+    const map = new Map<bigint, ByteString>()
+
+    const stateMap = new HashedMapState(new HashedMap(map))
+
+    await stateMap.connect(signer)
+
+    // deploy
+    const deployTx = await stateMap.deploy(1)
+    console.log('contract deployed: ', deployTx.id)
+
+    //call
+    const {
+        tx: tx1,
+        next: { instance: instance1 },
+    } = await insert(map, stateMap, 1n, toByteString('0001'))
+
+    console.log('contract insert called: ', tx1.id)
+    const {
+        tx: tx2,
+        next: { instance: instance2 },
+    } = await insert(map, instance1, 2n, toByteString('0002'))
+    console.log('contract insert called: ', tx2.id)
+    const {
+        tx: tx3,
+        next: { instance: instance3 },
+    } = await canGet(map, instance2, 2n, toByteString('0002'))
+    console.log('contract canGet called: ', tx3.id)
+    const {
+        tx: tx4,
+        next: { instance: instance4 },
+    } = await canGet(map, instance3, 1n, toByteString('0001'))
+    console.log('contract canGet called: ', tx4.id)
+    const {
+        tx: tx5,
+        next: { instance: instance5 },
+    } = await update(map, instance4, 1n, toByteString('000001'))
+    console.log('contract update called: ', tx5.id)
+    const {
+        tx: tx6,
+        next: { instance: instance6 },
+    } = await update(map, instance5, 2n, toByteString('000002'))
+    console.log('contract update called: ', tx6.id)
+    const {
+        tx: tx7,
+        next: { instance: instance7 },
+    } = await canGet(map, instance6, 2n, toByteString('000002'))
+    console.log('contract canGet called: ', tx7.id)
+    const {
+        tx: tx8,
+        next: { instance: instance8 },
+    } = await canGet(map, instance7, 1n, toByteString('000001'))
+    console.log('contract canGet called: ', tx8.id)
+    const {
+        tx: tx9,
+        next: { instance: instance9 },
+    } = await deleteKey(map, instance8, 1n, toByteString('000001'))
+    console.log('contract delete called: ', tx9.id)
+    const {
+        tx: tx10,
+        next: { instance: _ },
+    } = await notExist(map, instance9, 1n, toByteString('000001'))
+    console.log('contract notExist called: ', tx10.id)
+}
+
+main().catch((e) => {
+    console.log('error', e.message)
+})
