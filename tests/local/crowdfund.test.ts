9b6dd86559f1f88c22bfe4a1a3f5505ac1001cac
aaron67 <aaron67@aaron67.cc>
2023-02-13 22:50:56 +0800

    Fix crowdfund

--- tests/local/crowdfund.test.ts
+++ tests/local/crowdfund.test.ts
@@ -1,140 +1,73 @@
 import { expect } from 'chai'
-import { PubKey, PubKeyHash, Sig, bsv, toHex } from 'scrypt-ts'
+import { findSig, MethodCallOptions, PubKey, toHex } from 'scrypt-ts'
 import { Crowdfund } from '../../src/contracts/crowdfund'
-import { inputIndex, dummyUTXO } from './util/txHelper'
+import {
+    dummySigner,
+    dummyUTXO,
+    inputSatoshis,
+    randomPrivateKey,
+} from './util/txHelper'
 
-const privateKeyRecipient = bsv.PrivateKey.fromRandom('testnet')
-const pkhRecipient = bsv.crypto.Hash.sha256ripemd160(
-    privateKeyRecipient.publicKey.toBuffer()
-)
-
-const privateKeyContributor = bsv.PrivateKey.fromRandom('testnet')
-const privateKeyPublicKey = bsv.PublicKey.fromPrivateKey(privateKeyContributor)
+const [privateKeyRecipient, publicKeyRecipient, ,] = randomPrivateKey()
+const [privateKeyContributor, publicKeyContributor, ,] = randomPrivateKey()
 
 describe('Test SmartContract `Crowdfund`', () => {
+    // JS timestamps are in milliseconds, so we divide by 1000 to get UNIX timestamp
+    const deadline = Math.round(new Date('2020-01-03').valueOf() / 1000)
+    const target = BigInt(inputSatoshis - 100)
+
+    let crowdfund: Crowdfund
+
     before(async () => {
         await Crowdfund.compile()
+        crowdfund = new Crowdfund(
+            PubKey(toHex(publicKeyRecipient)),
+            PubKey(toHex(publicKeyContributor)),
+            BigInt(deadline),
+            target
+        )
+        await crowdfund.connect(
+            dummySigner([privateKeyRecipient, privateKeyContributor])
+        )
     })
 
     it('should collect fund success', async () => {
-        const oneDayAgo = new Date('2020-01-03')
-
-        // JS timestamps are in milliseconds so we divide by 1000 to get an UNIX timestamp
-        const deadline = Math.round(oneDayAgo.valueOf() / 1000)
-
-        const crowdfund = new Crowdfund(
-            PubKeyHash(toHex(pkhRecipient)),
-            PubKey(toHex(privateKeyPublicKey)),
-            BigInt(deadline),
-            10000n
+        const { tx: callTx, atInputIndex } = await crowdfund.methods.collect(
+            (sigResps) => findSig(sigResps, publicKeyRecipient),
+            {
+                fromUTXO: dummyUTXO,
+                pubKeyOrAddrToSign: publicKeyRecipient,
+                changeAddress: publicKeyRecipient.toAddress('testnet'),
+            } as MethodCallOptions<Crowdfund>
         )
-
-        const utxos = [dummyUTXO]
-
-        // construct a transaction for deployment
-        const deployTx = crowdfund.getDeployTx(utxos, 1)
-
-        const raisedAmount = 10000n
-        const callTx = crowdfund.getCallCollectTx(
-            deployTx,
-            PubKeyHash(toHex(pkhRecipient)),
-            raisedAmount
-        )
-
-        crowdfund.to = { tx: callTx, inputIndex }
-        const result = crowdfund.verify((self) => {
-            self.collect(raisedAmount)
-        })
-
-        expect(result.success).to.be.true
+        const result = callTx.verifyInputScript(atInputIndex)
+        expect(result.success, result.error).to.eq(true)
     })
 
-    it('should collect fund fail if  raisedAmount not reach target', async () => {
-        const oneDayAgo = new Date('2020-01-03')
-
-        const deadline = Math.round(oneDayAgo.valueOf() / 1000)
-
-        const crowdfund = new Crowdfund(
-            PubKeyHash(toHex(pkhRecipient)),
-            PubKey(toHex(privateKeyPublicKey)),
-            BigInt(deadline),
-            10000n
-        )
-
-        const utxos = [dummyUTXO]
-
-        // construct a transaction for deployment
-        const deployTx = crowdfund.getDeployTx(utxos, 1)
-
-        const raisedAmount = 100n
-
-        expect(() => {
-            crowdfund.getCallCollectTx(
-                deployTx,
-                PubKeyHash(toHex(pkhRecipient)),
-                raisedAmount
-            )
-        }).to.throw(/Execution failed/)
-    })
-
-    it('should success when refund ', async () => {
-        const deadline = Math.round(new Date('2020-01-03').valueOf() / 1000)
-
-        const crowdfund = new Crowdfund(
-            PubKeyHash(toHex(pkhRecipient)),
-            PubKey(toHex(privateKeyPublicKey)),
-            BigInt(deadline),
-            10000n
-        )
-
-        const utxos = [dummyUTXO]
-
-        // construct a transaction for deployment
-        const deployTx = crowdfund.getDeployTx(utxos, 10000)
-
+    it('should success when refund', async () => {
         const today = Math.round(new Date().valueOf() / 1000)
-
-        const callTx = crowdfund.getCallRefundTx(
-            deployTx,
-            PubKeyHash(toHex(pkhRecipient)),
-            privateKeyContributor,
-            today
+        const { tx: callTx, atInputIndex } = await crowdfund.methods.refund(
+            (sigResps) => findSig(sigResps, publicKeyContributor),
+            {
+                fromUTXO: dummyUTXO,
+                pubKeyOrAddrToSign: publicKeyContributor,
+                lockTime: today,
+            } as MethodCallOptions<Crowdfund>
         )
-
-        crowdfund.to = { tx: callTx, inputIndex }
-        const result = crowdfund.verify((self) => {
-            self.refund(Sig(callTx.getSignature(0) as string))
-        })
-
-        expect(result.success).to.be.true
+        const result = callTx.verifyInputScript(atInputIndex)
+        expect(result.success, result.error).to.eq(true)
     })
 
-    it('should fail when refund before deadline ', async () => {
-        const deadline = Math.round(new Date('2020-01-03').valueOf() / 1000)
-
-        const crowdfund = new Crowdfund(
-            PubKeyHash(toHex(pkhRecipient)),
-            PubKey(toHex(privateKeyPublicKey)),
-            BigInt(deadline),
-            10000n
-        )
-
-        const utxos = [dummyUTXO]
-
-        // construct a transaction for deployment
-        const deployTx = crowdfund.getDeployTx(utxos, 10000)
-
-        const beforeDeadline = Math.round(
-            new Date('2020-01-01').valueOf() / 1000
-        )
-
-        expect(() => {
-            crowdfund.getCallRefundTx(
-                deployTx,
-                PubKeyHash(toHex(pkhRecipient)),
-                privateKeyContributor,
-                beforeDeadline
+    it('should fail when refund before deadline', async () => {
+        return expect(
+            crowdfund.methods.refund(
+                (sigResps) => findSig(sigResps, publicKeyContributor),
+                {
+                    fromUTXO: dummyUTXO,
+                    pubKeyOrAddrToSign: publicKeyContributor,
+                    lockTime: deadline - 1,
+                } as MethodCallOptions<Crowdfund>
             )
-        }).to.throw(/Execution failed/)
+        ).to.be.rejectedWith(/fundraising expired/)
     })
 })
