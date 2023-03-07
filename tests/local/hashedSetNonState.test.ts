8e6a5bfb03445cd9d7c6b0eb24ae6c29d9e0f4ba
aaron67 <aaron67@aaron67.cc>
2023-03-07 14:15:55 +0800

    Fix #55

--- tests/local/hashedSetNonState.test.ts
+++ tests/local/hashedSetNonState.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { HashedSetNonState } from '../../src/contracts/hashedSetNonState'
-import { HashedSet } from 'scrypt-ts'
+import { HashedSet, MethodCallOptions } from 'scrypt-ts'
+import { getDummySigner, dummyUTXO } from './util/txHelper'
 
 describe('Test SmartContract `HashedSetNonState`', () => {
     before(async () => {
@@ -9,38 +10,56 @@ describe('Test SmartContract `HashedSetNonState`', () => {
 
     it('should unlock contract `HashedSetNonState` successfully.', async () => {
         const set = new HashedSet<bigint>()
-
         set.add(1n)
         set.add(2n)
         set.add(3n)
 
         const hashedSetNonState = new HashedSetNonState(set)
+        await hashedSetNonState.connect(getDummySigner())
 
-        let result = hashedSetNonState.verify(() => hashedSetNonState.add(1n))
+        const { tx: tx1, atInputIndex: atInputIndex1 } =
+            await hashedSetNonState.methods.add(1n, {
+                fromUTXO: dummyUTXO,
+            } as MethodCallOptions<HashedSetNonState>)
+        let result = tx1.verifyScript(atInputIndex1)
         expect(result.success, result.error).to.eq(true)
 
-        result = hashedSetNonState.verify(() => hashedSetNonState.add(7n))
+        const { tx: tx2, atInputIndex: atInputIndex2 } =
+            await hashedSetNonState.methods.add(7n, {
+                fromUTXO: dummyUTXO,
+            } as MethodCallOptions<HashedSetNonState>)
+        result = tx2.verifyScript(atInputIndex2)
         expect(result.success, result.error).to.eq(true)
     })
 
     it('should delete element successfully.', async () => {
         const set = new HashedSet<bigint>()
-
         set.add(1n)
-        set.add(2n)
-        set.add(10n)
 
         const hashedSetNonState = new HashedSetNonState(set)
+        await hashedSetNonState.connect(getDummySigner())
 
-        let result = hashedSetNonState.verify(() =>
-            hashedSetNonState.delete(2n)
+        const { tx, atInputIndex } = await hashedSetNonState.methods.delete(
+            1n,
+            {
+                fromUTXO: dummyUTXO,
+            } as MethodCallOptions<HashedSetNonState>
         )
+        const result = tx.verifyScript(atInputIndex)
         expect(result.success, result.error).to.eq(true)
+    })
 
-        expect(() => {
-            result = hashedSetNonState.verify(() =>
-                hashedSetNonState.delete(3n)
-            )
-        }).to.throw(/hashedSet should have the key before delete/)
+    it('should throw', async () => {
+        const set = new HashedSet<bigint>()
+        set.add(1n)
+
+        const hashedSetNonState = new HashedSetNonState(set)
+        await hashedSetNonState.connect(getDummySigner())
+
+        return expect(
+            hashedSetNonState.methods.delete(2n, {
+                fromUTXO: dummyUTXO,
+            } as MethodCallOptions<HashedSetNonState>)
+        ).to.be.rejectedWith(/hashedSet should have the key before delete/)
     })
 })
