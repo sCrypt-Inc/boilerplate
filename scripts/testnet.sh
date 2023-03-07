dbf1f24afd19108405e514079909593dc6087876
hh <hh@scrypt.io>
2023-03-07 15:17:49 +0800

    Fix shell error on linux

--- scripts/testnet.sh
+++ scripts/testnet.sh
@@ -5,33 +5,51 @@ day_mod=`expr $(date +%d) % 9`
 
 echo "day_mod: $day_mod"
 
-if [ $day_mod == 0 ]; then 
-    echo "run on linux, nodejs=16";
-    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_NODE_VERSION" == "16" ] ; then npm run testnet; fi
-elif [ $day_mod == 1 ]; then
-    echo "run on linux, nodejs=18";
-    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_NODE_VERSION" == "18" ] ; then npm run testnet; fi
-elif [ $day_mod == 2 ]; then
-    echo "run on linux, nodejs=19";
-    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_NODE_VERSION" == "19" ] ; then npm run testnet; fi
-elif [ $day_mod == 3 ]; then
-    echo "run on osx, nodejs=16";
-    if [ "$TRAVIS_OS_NAME" == "osx" ] && [ "$TRAVIS_NODE_VERSION" == "16" ] ; then npm run testnet; fi
-elif [ $day_mod == 4 ]; then
-    echo "run on osx, nodejs=18";
-    if [ "$TRAVIS_OS_NAME" == "osx" ] && [ "$TRAVIS_NODE_VERSION" == "18" ] ; then npm run testnet; fi
-elif [ $day_mod == 5 ]; then
-    echo "run on osx, nodejs=19";
-    if [ "$TRAVIS_OS_NAME" == "osx" ] && [ "$TRAVIS_NODE_VERSION" == "19" ] ; then npm run testnet; fi
-elif [ $day_mod == 6 ]; then
-    echo "run on windows, nodejs=16";
-    if [ "$TRAVIS_OS_NAME" == "windows" ] && [ "$TRAVIS_NODE_VERSION" == "16" ] ; then npm run testnet; fi
-elif [ $day_mod == 7 ]; then
-    echo "run on windows, nodejs=18";
-    if [ "$TRAVIS_OS_NAME" == "windows" ] && [ "$TRAVIS_NODE_VERSION" == "18" ] ; then npm run testnet; fi
-elif [ $day_mod == 8 ]; then
-    echo "run on windows, nodejs=19";
-    if [ "$TRAVIS_OS_NAME" == "windows" ] && [ "$TRAVIS_NODE_VERSION" == "19" ] ; then npm run testnet; fi
+if [ $day_mod -eq 0 ]; then 
+    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_NODE_VERSION" == "16" ] ; then 
+        echo "run on linux, nodejs=16";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 1 ]; then
+    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_NODE_VERSION" == "18" ] ; then
+        echo "run on linux, nodejs=18";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 2 ]; then
+    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_NODE_VERSION" == "19" ] ; then
+        echo "run on linux, nodejs=19";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 3 ]; then
+    if [ "$TRAVIS_OS_NAME" == "osx" ] && [ "$TRAVIS_NODE_VERSION" == "16" ] ; then
+        echo "run on osx, nodejs=16";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 4 ]; then
+    if [ "$TRAVIS_OS_NAME" == "osx" ] && [ "$TRAVIS_NODE_VERSION" == "18" ] ; then
+        echo "run on osx, nodejs=18";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 5 ]; then
+    if [ "$TRAVIS_OS_NAME" == "osx" ] && [ "$TRAVIS_NODE_VERSION" == "19" ] ; then
+        echo "run on osx, nodejs=19";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 6 ]; then
+    if [ "$TRAVIS_OS_NAME" == "windows" ] && [ "$TRAVIS_NODE_VERSION" == "16" ] ; then
+        echo "run on windows, nodejs=16";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 7 ]; then
+    if [ "$TRAVIS_OS_NAME" == "windows" ] && [ "$TRAVIS_NODE_VERSION" == "18" ] ; then
+        echo "run on windows, nodejs=18";
+        npm run testnet;
+    fi
+elif [ $day_mod -eq 8 ]; then
+    if [ "$TRAVIS_OS_NAME" == "windows" ] && [ "$TRAVIS_NODE_VERSION" == "19" ] ; then
+        echo "run on windows, nodejs=19";
+        npm run testnet;
+    fi
 else 
     echo "day_mod: error"
     exit -1;
