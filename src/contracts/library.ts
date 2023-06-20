0f4997e4f508f6ec1591d3e953d5147a48915a4c
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-06-20 14:58:08 +0100

    Add library

---
+++ src/contracts/library.ts
@@ -0,0 +1,38 @@
+
+
+export class L extends SmartContractLib{
+
+@prop()
+x : bigint
+
+constructor(a : bigint, b : bigint){
+   super(...arguments)
+   this.x = a + b
+
+}
+
+@method()
+f () : bigint{
+return this.x
+}
+
+}
+
+export class Test extends SmartContract{
+@prop()
+x : bigint
+
+@prop()
+l : L
+constructor(x:bigint, l:L){
+
+super(...arguments)
+this.x = x
+this.l = l
+}
+
+  @method()
+  public unlock(x : bigint){
+  assert(this.l.f() == x + this.x,'not equal')
+  }
+}
