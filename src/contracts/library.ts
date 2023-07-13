3569e00f64778efdc5a619541f5e4d0f9d1c80a2
hh <hf.zheng@scrypt.io>
2023-07-13 22:10:50 +0800

    Mirror fix

--- src/contracts/library.ts
+++ src/contracts/library.ts
@@ -1,48 +1,35 @@
-import {assert} from 'console'
-import {
-   prop,
-   SmartContractLib,
-   SmartContract,
-   method} from 'scrypt-ts'
+import { assert } from 'console'
+import { prop, SmartContractLib, SmartContract, method } from 'scrypt-ts'
 
-export class L extends SmartContractLib{
+export class L extends SmartContractLib {
+    @prop()
+    x: bigint
 
-@prop()
-x : bigint
-@prop()
-a : bigint
-@prop()
-b : bigint
+    constructor(a: bigint, b: bigint) {
+        super(...arguments)
+        this.x = a + b
+    }
 
-
-constructor(x : bigint, a : bigint, b : bigint){
-   super(...arguments)
-   this.x = a + b
-
+    @method()
+    f(): bigint {
+        return this.x
+    }
 }
 
-@method()
-f () : bigint{
-return this.x
-}
+export class Test extends SmartContract {
+    @prop()
+    x: bigint
 
-}
+    @prop()
+    l: L
+    constructor(x: bigint, l: L) {
+        super(...arguments)
+        this.x = x
+        this.l = l
+    }
 
-export class Test extends SmartContract{
-@prop()
-x : bigint
-
-@prop()
-l : L
-constructor(x:bigint, l:L){
-
-super(...arguments)
-this.x = x
-this.l = l
-}
-
-  @method()
-  public unlock(x : bigint){
-  assert(this.l.f() == x + this.x,'not equal')
-  }
+    @method()
+    public unlock(x: bigint) {
+        assert(this.l.f() == x + this.x, 'not equal')
+    }
 }
