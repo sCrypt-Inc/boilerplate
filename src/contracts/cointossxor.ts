d714484ea82984a70562f63abfcd70ac527b7b6c
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-07-18 12:26:03 +0100

    add cointossxor.ts

---
+++ src/contracts/cointossxor.ts
@@ -0,0 +1,44 @@
+import { assert } from "console";
+import { ByteString, PubKey, Sha256, Sig, SmartContract, hash256, len, method, prop, toByteString } from "scrypt-ts";
+
+export class CoinToss extends SmartContract{
+    @prop()
+    alice : PubKey
+    @prop()
+    bob : PubKey
+    @prop()
+    aliceHash : Sha256
+    @prop()
+    bobHash : Sha256
+
+    constructor(alice : PubKey, bob : PubKey, aliceHash : Sha256, bobHash : Sha256){
+        super(...arguments)
+        this.alice = alice
+        this.bob = bob
+        this.aliceHash = aliceHash
+        this.bobHash = bobHash
+    }
+
+    @method()
+    public toss(aliceNonce : ByteString, bobNonce : ByteString, sig : Sig){
+        // nonce can be of any lenght as long as its resistant to brute-force attack
+        // we use 256 bits / 32 bytes as an example here
+
+        assert(len(aliceNonce) == 32)
+        assert(hash256(aliceNonce) == this.aliceHash)
+        assert(len(bobNonce) == 32)
+        assert(hash256(bobNonce) == this.bobHash)
+
+        // last bit of XOR
+
+        const head : ByteString = (BigInt(aliceNonce) ^ BigInt(bobNonce)) && toByteString('0000000000000000000000000000000000000000000000000000000000000001')
+
+        // head -> Alice; tails bob -> wins
+
+        const winner : PubKey = head ? this.alice : this.bob
+
+        // check winner signature
+
+        assert(this.checkSig(sig, winner))
+    }
+}
