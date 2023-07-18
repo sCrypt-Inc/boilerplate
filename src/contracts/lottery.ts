0af37db63181e02def74e546eb3fa3a8bbb8523c
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-07-18 10:21:23 +0100

    add lottery.ts

---
+++ src/contracts/lottery.ts
@@ -0,0 +1,41 @@
+// a fair secure multiparty lottery
+// each player i choose a random number n_i and the winner is the w-th player
+// where w = (n_0 + n_1 + ... + n_(N-1)) mod N
+
+import { assert } from "console";
+import { FixedArray, PubKey, Sha256, Sig, SmartContract, hash256, int2ByteString, method, prop } from "scrypt-ts";
+
+
+export class Lottery extends SmartContract{
+    // number of players
+    @prop()
+    static readonly N : bigint = 5n
+    @prop()
+    readonly players : FixedArray<PubKey, 5>
+    
+    @prop()
+    readonly nonceHashes : FixedArray<Sha256, 5>
+
+    constructor(players : FixedArray<PubKey, 5>, nonceHashes : FixedArray<Sha256, 5>){
+        super(...arguments)
+        this.players = players
+        this.nonceHashes = nonceHashes
+    }
+
+    @method()
+    public reveal(nonce : FixedArray<number, 5>, sig : Sig){
+        let i = 0
+        let sum = 0
+
+        for(let i = 0; i < 5; i ++){
+            assert(hash256(int2ByteString(BigInt(nonce[i]))) == this.nonceHashes[i])
+
+            sum += nonce[i]
+            i++ 
+        }
+
+        const winner : PubKey = this.players[sum % 5]
+
+        assert(this.checkSig(sig, winner))
+    }
+}
