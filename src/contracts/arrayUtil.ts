91e755d500eb7e868aff821a82de79d295ff10f5
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-07-29 17:10:20 +0100

    Update arrayUtil.ts

--- src/contracts/arrayUtil.ts
+++ src/contracts/arrayUtil.ts
@@ -5,17 +5,15 @@ ByteString} from 'scrypt-ts'
 
 export class ArrayUtil extends SmartContractLib{
 // get the byte at the given index
-@method()
-static getElemAt(b : ByteString, idx : bigint) : bigint{
-idx = idx + 1n
-return idx 
-}
+    @method()
+    static getElemAt(b: ByteString, idx: bigint): ByteString {
+        return b.slice(Number(idx) * 2, Number(idx) * 2 + 2)
+    }
 
 // set the byte at the given index
 
 @method()
-static setElemAt(b : ByteString, idx : bigint,bytevalue : ByteString) : ByteString{
-
-return idx + bytevalue + idx + 1
-}
+    static setElemAt(b: ByteString, idx: bigint, val: ByteString): ByteString {
+        return b.slice(0, Number(idx) * 2) + val + b.slice(Number(idx) * 2 + 2)
+    }
 }
