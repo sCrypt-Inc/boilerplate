640451fba6a2aa694244a6db40f10675b365aa4f
hh <hf.zheng@scrypt.io>
2023-08-02 09:01:32 +0800

    Fix symbol  duplicate

--- src/contracts/rabin.ts
+++ src/contracts/rabin.ts
@@ -1,31 +1,27 @@
 // You can read about this contract at :
 // https://en.wikipedia.org/wiki/Rabin_signature_algorithm
-import { ByteString, SmartContractLib, Utils, hash256, len, method, sha256 } from "scrypt-ts"
+import { ByteString, SmartContractLib, Utils, method, sha256 } from 'scrypt-ts'
 
-//Rabin Public Key
-type RabinPubKey = bigint
+import { RabinSig, RabinPubKey } from 'scrypt-ts-lib'
 
-//Rabin signature is combination of (S, U).
+export class RabinSignature extends SmartContractLib {
+    @method()
+    static checkSig(
+        msg: ByteString,
+        sig: RabinSig,
+        pubkey: RabinPubKey
+    ): boolean {
+        const h: bigint = Utils.fromLEUnsigned(
+            RabinSignature.hash(msg + sig.padding)
+        )
+        return (sig.s * sig.s) % pubkey == h % pubkey
+    }
 
-type RabinSig = {
-    s : bigint  // S
-    padding : ByteString //U
-}
-    export class RabinSignature extends SmartContractLib{
+    @method()
+    static hash(x: ByteString): ByteString {
+        // expand in to 512 bit hash
 
-        @method()
-        static checkSig(msg : ByteString, sig : RabinSig, pubkey : RabinPubKey) : boolean {
-            const h : bigint = Utils.fromLEUnsigned(RabinSignature.hash(msg + sig.padding))
-            return (sig.s * sig.s) % pubkey == h % pubkey
-        }
-        
-        @method()
-        static hash(x : ByteString) : ByteString{
-            // expand in to 512 bit hash
-
-            const hx = sha256(x)
-            return sha256(hx.slice(0, 32)) + sha256(hx.slice(32, 64))
-
-        }
-
+        const hx = sha256(x)
+        return sha256(hx.slice(0, 32)) + sha256(hx.slice(32, 64))
+    }
 }
