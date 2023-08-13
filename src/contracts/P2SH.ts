8b79b00e679c24374083f7eb2d424f2a31687027
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-08-13 10:18:59 +0100

    add P2SH

---
+++ src/contracts/P2SH.ts
@@ -0,0 +1,23 @@
+import { ByteString, PubKeyHash, SmartContract, Utils, assert, hash160, hash256, method, prop } from "scrypt-ts";
+
+export class P2SH extends SmartContract{
+    @prop()
+    scripthash : PubKeyHash
+
+    constructor(scripthash : PubKeyHash){
+        super(...arguments)
+        this.scripthash = scripthash
+    }
+
+    @method()
+    public redeem(redeemScript : ByteString){
+        assert(hash160(redeemScript) == this.scripthash)
+
+        const amount : bigint = this.ctx.utxo.value
+
+        // use redeem script as the new locking script, whithou changing the utxo balance
+        const output : ByteString = Utils.buildOutput(redeemScript, amount)
+
+        assert(hash256(output) == this.ctx.hashOutputs)
+    }
+}
