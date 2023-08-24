6487d4e86de4a5f9a80d38c0343ffe566c0cfab2
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-08-24 20:44:46 +0100

    Add Modular Exponentiation test

---
+++ tests/modEXP.test.ts
@@ -0,0 +1,35 @@
+import { expect, use } from 'chai'
+import { MethodCallOptions} from 'scrypt-ts'
+import { ModExp } from '../src/contracts/modEXP'
+import { getDefaultSigner } from './utils/txHelper'
+import chaiAsPromised from 'chai-as-promised'
+import { describe } from 'mocha'
+
+use(chaiAsPromised)
+
+describe('Test SmartContract `modEXP`', () => {
+    let instance: ModExp
+    before(async () => {
+        await ModExp.compile()
+        
+        instance = new ModExp(13n)
+        await instance.connect(getDefaultSigner())
+    })
+
+    it('should pass the public method unit test successfully.', async () => {
+        const deployTx = await instance.deploy(1)
+        console.log('modEXP contract deployed: ', deployTx.id)
+
+        
+        const { tx: callTx, atInputIndex } = await instance.methods.main(
+            2n, 3n, 8n
+            
+        )
+        console.log('`modEXP` contract called : ', callTx.id)
+
+        const result = callTx.verifyScript(atInputIndex)
+        expect(result.success, result.error).to.eq(true)
+        
+    })
+
+})
