d246f9fab5115c8f6bad3ee31434f2c6334b530d
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-08-31 14:15:09 +0100

    add matrix test

---
+++ tests/matrix.test.ts
@@ -0,0 +1,32 @@
+import { Matrix } from '../src/contracts/matrix'
+import { getDefaultSigner} from './utils/txHelper'
+
+import { expect } from 'chai'
+
+async function main() {
+    await Matrix.compile()
+
+    // const [privateKeyalice, publicKeyalice] = randomPrivateKey()
+    // const [privateKeybob, publicKeybob] = randomPrivateKey()
+
+    const instance = new Matrix()
+
+    await instance.connect(getDefaultSigner())
+    const deployTx = await instance.deploy(1)
+    console.log(`Matrix contract deployed:  ${deployTx.id}`)
+
+    const callContract = async () => {
+        await instance.methods.main([
+            [1n, 1n, 1n, 1n], 
+            [2n, 2n, 2n, 2n], 
+            [3n, 3n, 3n, 3n], 
+            [4n, 4n, 4n, 4n]
+        ])
+        expect(callContract()).not.Throw
+    }
+}
+describe('Test SmartContract `Matrix`', () => {
+    it('should succeed', async () => {
+        await main()
+    })
+})
