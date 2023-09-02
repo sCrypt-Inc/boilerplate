32c6f937c73b045b7c9acba0bf6553cd7c521a22
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-09-02 12:08:29 +0100

    Update rps.test.ts

--- tests/rps.test.ts
+++ tests/rps.test.ts
@@ -1,24 +1,69 @@
+import { MethodCallOptions, PubKey, findSig, toHex } from 'scrypt-ts'
 import { RockPaperScissors } from '../src/contracts/rps'
 import { myAddress } from './utils/privateKey'
-import { getDefaultSigner} from './utils/helper'    
-import { expect } from 'chai'
+import { getDefaultSigner, randomPrivateKey} from './utils/helper'    
+import { expect, use } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+use(chaiAsPromised)
 
-async function main() {
-    await RockPaperScissors.compile()
+async function main(){
 
-    const instance = new RockPaperScissors()
+    const [privateKey, publickey] = randomPrivateKey()
 
-    await instance.connect(getDefaultSigner())
-    const deployTx = await instance.deploy(1)
-    console.log(`RockPaperScirssors contract deployed:  ${deployTx.id}`)
+        await RockPaperScissors.compile()
+
+        const instance = new RockPaperScissors()
+    
+        await instance.connect(getDefaultSigner(privateKey))
+    
+   
+    it('should pass the follow method successfully ', async () => {
+
+        await instance.deploy(10)
 
     const callContract = async () => {
-        await instance.methods.main(3n, myAddress, 12n)
-        expect(callContract()).not.Throw
+        await instance.methods.follow(3n, myAddress, 12n)
+       return expect(callContract()).not.be.rejected
     }
-}
-describe('Test SmartContract `RockPaperScirssors`', () => {
-    it('should succeed', async () => {
-        await main()
+
     })
+    it('should pass the finished method successfully ', async () => {
+
+        await instance.deploy(100)
+
+    const callContract = async () => {
+        await instance.methods.finished(3n, (sigResps) => findSig(sigResps, publickey) ,PubKey(toHex(publickey)), 2n,
+        {
+            pubKeyOrAddr : publickey
+        }as MethodCallOptions<RockPaperScissors>)
+       return expect(callContract()).not.be.rejected
+    }
+
+    })
+
+    it('should throw when calling follow ', async () => {
+
+        await instance.deploy(10)
+    
+    const callContract = async () => {
+        await instance.methods.follow(6n, myAddress, 12n)
+       return expect(callContract()).to.be.rejectedWith(/ follow method failed/)
+    }
+
+    })
+    it('should throw when calling finished ', async () => {
+
+        await instance.deploy(100)
+    const callContract = async () => {
+        await instance.methods.finished(6n, (sigResps) => findSig(sigResps, publickey) ,PubKey(toHex(publickey)), 2n,
+        {
+            pubKeyOrAddr : publickey
+        }as MethodCallOptions<RockPaperScissors>)
+       return expect(callContract()).to.be.rejectedWith(/ finish method failed/)
+    }
+
+    })
+}
+describe('Test SmartContract `RockPaperScirssors`', async () => {
+    await main()
 })
