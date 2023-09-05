98e5e4a23bceeb7b5e6e725ed81be448bda9b044
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-09-05 16:14:48 +0100

    Add base58 Test

---
+++ tests/Base58.test.ts
@@ -0,0 +1,27 @@
+import { toByteString } from 'scrypt-ts'
+import { Base58Test } from '../src/contracts/Base58'
+import { getDefaultSigner } from './utils/txHelper'
+import { expect, use } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+use(chaiAsPromised)
+
+async function main() {
+    await Base58Test.compile()
+
+    let instance = new Base58Test()
+
+    await instance.connect(getDefaultSigner())
+
+    it('should encode address correctly', async () => {
+       const deployTx =  await instance.deploy(1)
+        console.log('deployed ', deployTx.id)
+
+        const callContract = async () => {
+            await instance.methods.main(toByteString('1234567890abcdef', true))
+            return expect(callContract()).not.be.rejected
+        }
+    })
+}
+describe('Test SmartContract `Base58`', async () => {
+    await main()
+})
