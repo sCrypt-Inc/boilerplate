ad71ef6caee43ac0ad22e67bf8cb986ab86dc2c4
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-09-17 07:00:12 +0100

    Update CrowdfundStateful.ts

--- src/contracts/CrowdfundStateful.ts
+++ src/contracts/CrowdfundStateful.ts
@@ -10,6 +10,10 @@ import {
     Sig,
     SmartContract,
     Utils,
+    bsv,
+    MethodCallOptions,
+    ContractTransaction,
+    StatefulNext,
 } from 'scrypt-ts'
 
 /*
@@ -18,7 +22,6 @@ import {
  */
 
 export type DonorMap = HashedMap<PubKey, bigint>
-export type RefundMap = HashedMap<PubKey, boolean>
 
 export class CrowdfundStateful extends SmartContract {
     @prop()
@@ -27,13 +30,10 @@ export class CrowdfundStateful extends SmartContract {
     static readonly UINT_MAX: bigint = 0xffffffffn
 
     @prop()
-    readonly beneficiary: PubKey
+    beneficiary: PubKey
 
     @prop(true)
     donor: DonorMap
-
-    @prop(true)
-    donorRefunded: RefundMap
 
     @prop()
     readonly deadline: bigint
@@ -44,14 +44,12 @@ export class CrowdfundStateful extends SmartContract {
     constructor(
         beneficiary: PubKey,
         donor: DonorMap,
-        donorRefunded: RefundMap,
         deadline: bigint,
         target: bigint
     ) {
         super(...arguments)
         this.beneficiary = beneficiary
         this.donor = donor
-        this.donorRefunded = donorRefunded
         this.deadline = deadline
         this.target = target
     }
@@ -70,8 +68,19 @@ export class CrowdfundStateful extends SmartContract {
 
         this.donor.set(contributor, amount)
 
-        //setting refunded to false
-        this.donorRefunded.set(contributor, false)
+        //updating the contract state
+        let output: ByteString =
+            this.buildStateOutput(this.ctx.utxo.value + amount) +
+            this.buildChangeOutput()
+
+        assert(hash256(output) == this.ctx.hashOutputs, 'hashOutputs mismatch')
+    }
+
+    // Method to collect pledged fund.
+    @method()
+    public collect(sig: Sig) {
+        // Ensure the collected amount actually reaches the target.
+        assert(this.ctx.utxo.value >= this.target)
 
         assert(
             this.ctx.sequence < CrowdfundStateful.UINT_MAX,
@@ -88,38 +97,15 @@ export class CrowdfundStateful extends SmartContract {
         }
         assert(
             this.ctx.locktime >= this.deadline,
-            ' You cannot donate afterfundraising expired'
+            'the beneficiary cannot collect before fundraising expired'
         )
-
-        //updating the contract state
-        let output: ByteString = this.buildStateOutput(
-            this.ctx.utxo.value + amount
-        )
-
-        // handling change output
-        if (this.changeAmount > 0n) {
-            output += this.buildChangeOutput()
-        }
-
-        assert(hash256(output) == this.ctx.hashOutputs, 'hashOutputs mismatch')
-    }
-
-    // Method to collect pledged fund.
-    @method()
-    public collect(sig: Sig) {
-        // Ensure the collected amount actually reaches the target.
-        assert(this.ctx.utxo.value >= this.target)
 
         // Funds go to the beneficiary.
-        let outputs = Utils.buildPublicKeyHashOutput(
-            hash160(this.beneficiary),
-            this.ctx.utxo.value
-        )
-
-        // Handling change output.
-        if (this.changeAmount > 0n) {
-            outputs += this.buildChangeOutput()
-        }
+        let outputs =
+            Utils.buildPublicKeyHashOutput(
+                hash160(this.beneficiary),
+                this.ctx.utxo.value
+            ) + this.buildChangeOutput()
 
         assert(hash256(outputs) == this.ctx.hashOutputs, 'hashOutputs mismatch')
         // Validate signature of beneficiary
@@ -132,41 +118,102 @@ export class CrowdfundStateful extends SmartContract {
     // donors can be refunded after the deadline.
     @method()
     public refund(contributor: PubKey, amount: bigint, sig: Sig) {
-        assert(this.donor.canGet(contributor, amount))
-
-        // make sure it has not refunded before
-        assert(
-            this.donorRefunded.canGet(contributor, false),
-            'already refunded'
-        )
-
-        
-        // setting it as refunded
-        this.donorRefunded.set(contributor, true)
-        
-        // update state
-        const output: ByteString = this.buildStateOutput(
-            this.ctx.utxo.value - amount
-        )
-
         // checking the validity of the signature
         assert(this.checkSig(sig, contributor), 'donor signature check failed')
 
-        // refund output
-        const refund = Utils.buildPublicKeyHashOutput(
-            hash160(contributor),
-            amount
-        )
+        assert(this.donor.canGet(contributor, amount), 'not denoted before')
 
-        let outputs = output + refund
+        // removing donor from the donators list
+        assert(this.donor.delete(contributor), 'removing donator failed')
 
-        if (this.changeAmount > 0n) {
-            outputs += this.buildChangeOutput()
+        // update state outputs
+        const output =
+            this.buildStateOutput(this.ctx.utxo.value - amount) +
+            Utils.buildPublicKeyHashOutput(hash160(contributor), amount) +
+            this.buildChangeOutput()
+
+        // console.log('outputs ', this.debug.diffOutputs(outputs))
+        assert(hash256(output) == this.ctx.hashOutputs, 'hashOutput Misssmatch')
+    }
+
+    // User defined transaction builder for calling function `collect`
+    static async buildTxForCollect(
+        current: CrowdfundStateful,
+        options: MethodCallOptions<CrowdfundStateful>
+    ): Promise<ContractTransaction> {
+        const defaultChangeAddress = await current.signer.getDefaultAddress()
+        const unsignedTx: bsv.Transaction = new bsv.Transaction()
+            // add contract input
+            .addInput(current.buildContractInput(options.fromUTXO))
+
+            // build collect output
+            .addOutput(
+                new bsv.Transaction.Output({
+                    script: bsv.Script.fromHex(
+                        Utils.buildPublicKeyHashScript(
+                            hash160(current.beneficiary)
+                        )
+                    ),
+                    satoshis: current.balance,
+                })
+            )
+            // build change output
+            .change(options.changeAddress || defaultChangeAddress)
+
+            .setInputSequence(0, options.sequence || 0xffffffff - 1)
+            .setLockTime(options.lockTime || 0)
+
+        return {
+            tx: unsignedTx,
+            atInputIndex: 0,
+            nexts: [],
         }
+    }
 
-        assert(
-            hash256(outputs) == this.ctx.hashOutputs,
-            'hashOutput Misssmatch'
-        )
+    // User defined transaction builder for calling function `refund`
+    static async buildTxForRefund(
+        current: CrowdfundStateful,
+        options: MethodCallOptions<CrowdfundStateful>,
+        contributor: PubKey,
+        amount: bigint
+    ): Promise<ContractTransaction> {
+        const defaultChangeAddress = await current.signer.getDefaultAddress()
+        const balance = current.balance - Number(amount)
+        const nextInstance = current.next()
+        nextInstance.donor.delete(contributor)
+
+        const unsignedTx: bsv.Transaction = new bsv.Transaction()
+            // add contract input
+            .addInput(current.buildContractInput(options.fromUTXO))
+            // build next instance output
+            .addOutput(
+                new bsv.Transaction.Output({
+                    script: nextInstance.lockingScript,
+                    satoshis: balance,
+                })
+            )
+            // build refund output
+            .addOutput(
+                new bsv.Transaction.Output({
+                    script: bsv.Script.fromHex(
+                        Utils.buildPublicKeyHashScript(hash160(contributor))
+                    ),
+                    satoshis: Number(amount),
+                })
+            )
+            // build change output
+            .change(options.changeAddress || defaultChangeAddress)
+
+        return {
+            tx: unsignedTx,
+            atInputIndex: 0,
+            nexts: [
+                {
+                    instance: nextInstance,
+                    atOutputIndex: 0,
+                    balance: balance,
+                },
+            ],
+        }
     }
 }
