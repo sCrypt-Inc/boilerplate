c38aa1b70248ccd921d4393c2d01b7d2d51f126b
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-10-07 20:35:09 +0100

    Add Todo List contract

---
+++ src/contracts/todoList.ts
@@ -0,0 +1,52 @@
+import {
+    SmartContract,
+    ByteString,
+    prop,
+    method,
+    assert,
+    FixedArray,
+    fill,
+    toByteString,
+    hash256,
+} from 'scrypt-ts'
+
+export type Task = {
+    task: ByteString
+    isCompleted: boolean
+}
+export class TodoList extends SmartContract {
+    static readonly MAX_TASKCOUNT = 10
+    @prop(true)
+    tasks: FixedArray<Task, typeof TodoList.MAX_TASKCOUNT>
+
+    constructor() {
+        super(...arguments)
+        this.tasks = fill(
+            {
+                task: toByteString(''),
+                isCompleted: false,
+            },
+            TodoList.MAX_TASKCOUNT
+        )
+    }
+
+    @method()
+    public addTask(task: Task, taskIdx: bigint) {
+        task = this.tasks[Number(taskIdx)]
+        const output =
+            this.buildStateOutput(this.ctx.utxo.value) +
+            this.buildChangeOutput()
+        assert(hash256(output) == this.ctx.hashOutputs, 'HashOutput Mismatch')
+    }
+
+    @method()
+    public taskCompleted(taskIdx: bigint) {
+        const task = this.tasks[Number(taskIdx)]
+        this.tasks[Number(taskIdx)].isCompleted = true
+
+        const output =
+            this.buildStateOutput(this.ctx.utxo.value) +
+            this.buildChangeOutput()
+        assert(hash256(output) == this.ctx.hashOutputs, 'HashOutput Mismatch')
+    }
+}
