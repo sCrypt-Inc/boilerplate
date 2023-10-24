751c4ebfd2111beb54333dc590466e89fa0b4e3d
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-10-24 18:07:11 +0100

    Update callee.ts

--- src/contracts/callee.ts
+++ src/contracts/callee.ts
@@ -41,43 +41,43 @@ export class Callee extends SmartContract {
 
         assert(hash256(output) == this.ctx.hashOutputs)
     }
-    // static async buildTxForSolve(
-    //     current: Callee,
-    //     options: MethodCallOptions<Callee>,
-    //     co : Coeff,
-    //     x : bigint,
+    static async buildTxForSolve(
+        current: Callee,
+        options: MethodCallOptions<Callee>,
+        co : Coeff,
+        x : bigint,
         
-    // ): Promise<ContractTransaction> {
-    //     const defaultChangeAddress = await current.signer.getDefaultAddress()
-    //     const data: ByteString =
-    //     int2ByteString(co.a, Callee.N) +
-    //     int2ByteString(co.b, Callee.N) +
-    //     int2ByteString(co.c, Callee.N)
+    ): Promise<ContractTransaction> {
+        const defaultChangeAddress = await current.signer.getDefaultAddress()
+        const data: ByteString =
+        int2ByteString(co.a, Callee.N) +
+        int2ByteString(co.b, Callee.N) +
+        int2ByteString(co.c, Callee.N)
 
-    //     const outputScript: ByteString = Utils.buildOpreturnScript(data)
+        const outputScript: ByteString = Utils.buildOpreturnScript(data)
 
-    //     const unsignedTx: bsv.Transaction = new bsv.Transaction()
-    //         // add contract input
-    //         .addInput(current.buildContractInput(options.fromUTXO))
+        const unsignedTx: bsv.Transaction = new bsv.Transaction()
+            // add contract input
+            .addInput(current.buildContractInput(options.fromUTXO))
 
-    //         // build output
-    //         .addOutput(
-    //             new bsv.Transaction.Output({
-    //                 script: bsv.Script.fromHex(
-    //                     Utils.buildOpreturnScript(data)
-    //                 ),
-    //                 satoshis: current.balance,
-    //             })
-    //         )
+            // build output
+            .addOutput(
+                new bsv.Transaction.Output({
+                    script: bsv.Script.fromHex(
+                        Utils.buildOpreturnScript(data)
+                    ),
+                    satoshis: current.balance,
+                })
+            )
         
-    //         // build change output
-    //         .change(options.changeAddress || defaultChangeAddress)
+            // build change output
+            .change(options.changeAddress || defaultChangeAddress)
 
-    //     return {
-    //         tx: unsignedTx,
-    //         atInputIndex: 0,
-    //         nexts: [],
-    //     }
-    // }
+        return {
+            tx: unsignedTx,
+            atInputIndex: 0,
+            nexts: [],
+        }
+    }
 
 }
