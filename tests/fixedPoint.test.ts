62b3c8565dec43adf132e1b4ebdf53e1a2931ad9
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-10-26 00:02:19 +0100

    Update fixedPoint.test.ts

--- tests/fixedPoint.test.ts
+++ tests/fixedPoint.test.ts
@@ -1,26 +1,25 @@
-import { FPTest } from '../src/contracts/fixedPoint'
-import { getDefaultSigner } from './utils/helper'
+import { FPTest } from '../src/contracts/demo'
+import { getDefaultSigner } from './utils/txHelper'
 import { expect, use } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 use(chaiAsPromised)
 
 describe('Test SmartContract `FixedPoint`', () => {
- let instance : FPTest
+ let instance : FPTest;
+
  before(async () => {
     await FPTest.loadArtifact()
-
-    const instance = new FPTest()
-
-    await instance.connect(getDefaultSigner())
+    instance = new FPTest(); 
+    await instance.connect(getDefaultSigner()); 
 })
 
-    it('should unlock correctly ', async () => {
-        await instance.deploy(1)
+it('should unlock correctly ', async () => {
+    await instance.deploy(1)
 
-        const callContract = async () => {
-            await instance.methods.unlock(10n, 20, 30, 0n, 50n)
-            return expect(callContract()).not.be.rejected
-        }
-    })
+    const callContract = async () => {
+        await instance.methods.unlock(10n, 20, 30, 0n, 50n)
+        return expect(callContract()).not.be.rejected
+    }
+})
 
 })
