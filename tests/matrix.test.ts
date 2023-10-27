340f9e6adcd6ed606dffdd19865529d7d8dcd4ba
yusuf idi maina <120538505+yusufidimaina9989@users.noreply.github.com>
2023-10-27 10:48:05 +0100

    Update matrix.test.ts

--- tests/matrix.test.ts
+++ tests/matrix.test.ts
@@ -1,29 +1,27 @@
 import { Matrix } from '../src/contracts/matrix'
 import { getDefaultSigner } from './utils/helper'
-
 import { expect } from 'chai'
 
 describe('Test SmartContract `Matrix`', () => {
-  let instance : Matrix
-    before(async () => {
-    Matrix.loadArtifact()
+  let instance: Matrix;
 
-    const instance = new Matrix()
+  before(async () => {
+    await Matrix.loadArtifact(); 
+    instance = new Matrix();
+    await instance.connect(getDefaultSigner());
+  });
 
-    await instance.connect(getDefaultSigner())
-    })
+  it('should pass the public method successfully', async () => {
+    await instance.deploy(1);
 
-    it(' should pass the public method successfully', async () => {
-    await instance.deploy(1)
-    
     const callContract = async () => {
-        await instance.methods.main([
-            [1n, 1n, 1n, 1n],
-            [2n, 2n, 2n, 2n],
-            [3n, 3n, 3n, 3n],
-            [4n, 4n, 4n, 4n],
-        ])
-        expect(callContract()).not.Throw
+      await instance.methods.main([
+        [1n, 1n, 1n, 1n],
+        [2n, 2n, 2n, 2n],
+        [3n, 3n, 3n, 3n],
+        [4n, 4n, 4n, 4n],
+      ]);
+      expect(callContract).to.not.throw(); 
     }
-    })
-}) 
+  });
+});
